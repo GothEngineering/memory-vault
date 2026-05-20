04f73d505eb0afd217a2b8131e8ed172296c95a1
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-05-20 10:39:51 -0400

    Added the create button, the read button and the update button
    Also moved the script to a folder named "scripts"

---
+++ scripts/SQLMemories.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var title_text: TextEdit = $TitleText
+@onready var desc_text: TextEdit = $DescText
+
+var database : SQLite
+
+func _ready() -> void:
+	database = SQLite.new()
+	database.path = "res://memories_data.db"
+	database.open_db()
+	pass # TO DO: connect the delete data button and the custom select button
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_create_data_pressed() -> void:
+	var data = {
+		"title" : title_text.text,
+		"description" : desc_text.text,
+	}
+
+	database.insert_row("memories", data)
+	
+
+func _on_read_data_pressed() -> void:
+	print(database.select_rows("memories", "", ["*"]))
+	
+
+func _on_update_data_pressed() -> void:
+	database.update_rows("memories", "title = '" + title_text.text + "'", {"description": str(desc_text.text)})
+	
+
+
+func _on_delete_data_pressed() -> void:
+	pass
+
+
+func _on_custom_select_pressed() -> void:
+	pass 
