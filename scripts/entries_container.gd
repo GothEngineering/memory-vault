309f1495d8deb3c25d46dc6b9e0e8c96fb4cce3f
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-06-01 16:20:44 -0400

    Laying the groundwork for the double click system on the labels

--- scripts/entries_container.gd
+++ scripts/entries_container.gd
@@ -10,3 +10,8 @@ func _process(delta: float) -> void:
 
 func set_rows_text(new_text: String) -> void:
 	rich_text_label.text = new_text
+
+#func _gui_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton:
+		#if event.button_index == MOUSE_BUTTON_LEFT and event.double_click:
+			#print("oli toi probando")
