7d993d08f640dc34749065a597f558be03c65e6f
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-06-13 08:52:34 -0400

    Added the function to create a .db file if there's not one present

--- scripts/database_global.gd
+++ scripts/database_global.gd
@@ -1,11 +1,28 @@
 extends Node
 
 var database : SQLite
+var db_path = "user://memories_data.db"
 
 func _ready() -> void:
 	database = SQLite.new()
-	database.path = "res://memories_data.db"
-	database.open_db()
+	database.path = db_path
+	if not FileAccess.file_exists(db_path):
+		database.open_db()
+		create_the_tables()
+	else:
+		database.open_db()
+
+func create_the_tables():
+	var tables = {
+		"id" : {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment": true},
+		"title" : {"data_type":"text"},
+		"description" : {"data_type":"text"},
+		"game_title" : {"data_type":"text"},
+		"location" : {"data_type":"text"},
+		"feeling" : {"data_type":"text"},
+		"data_saved" : {"data_type":"text"},
+	}
+	database.create_table("memories", tables)
 
 
 func _process(delta: float) -> void:
