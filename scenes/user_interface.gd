4ff259661836d7b708ccdb3ab05c7ac9835875cf
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-06-16 08:28:40 -0400

    Improved the double click function

--- scenes/user_interface.gd
+++ scenes/user_interface.gd
@@ -79,6 +79,13 @@ func _clicked_entry_signal(selection):
 
 		entry_sorter.add_child(new_label)
 		new_label.set_rows_text(data_on_template)
+		
+		title_input.text = rows["title"]
+		desc_input.text = rows["description"]
+		game_title_input.text = rows["location"]
+		location_input.text = rows["location"]
+		feeling_input.text = rows["feeling"]
+
 
 # Entry creation
 func _on_create_data_pressed() -> void:
